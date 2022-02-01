0f1e97d5073e9bb77bf9a9e33df0ac0886fbc0c0
andrk0p <andrk0p@mail.ru>
2022-02-01 22:23:49 +0300

    no message

---
+++ src/DateTimePicker/Models/DateTimePickerOptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DateTimePicker.Models
+{
+	public struct DateTimePickerOptions
+	{
+		public DateTimePickerOptions(DateTime currentDate, int minuteStep)
+		{
+			if (minuteStep < 1 || minuteStep > 59)
+			{
+				throw new ArgumentException($"{nameof(minuteStep)} should have values from 1 to 59");
+			}
+
+			CurrentDate = currentDate;
+			MinuteStep = minuteStep;
+		}
+
+		public DateTime CurrentDate { get; set; }
+
+		public int MinuteStep { get; set; }
+	}
+}
+
